c44cdb2b670036a346910643057289cc699901e2
piero.silvestri <piero.silvestri@overit.it>
2024-01-29 17:42:12 +0100

    Added routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,11 +19,12 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  
+  /*
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+  */
 
   getHeroes(): void {
     this.heroService.getHeroes()
